94e0d6d47ded2987a8dd861e80c10cd568a39216
a3laxi <mad.aalachi@gmail.com>
2023-12-10 21:03:31 +0100

    CSSing

--- src/components/Apropos.js
+++ src/components/Apropos.js
@@ -9,11 +9,11 @@ const Apropos = () => {
             <ul className={styles.wrapper}>
                 <li>
                     <h3>Qui sommes-nous ?</h3>
-                    <div>Ephesus est une librairie en ligne qui met tout en oeuvre pour créer au travers de ses solutions de collecte, d'achat et de vente de livre neufs et d'occasion de la valeur pour ses clients, l'environnement et le social afin de lutter contre le gaspillage et promouvoir l'économie circulaire en donnant une seconde vie au livres.</div>
+                    <div><strong>Libres Livres</strong> est une librairie en ligne qui met tout en oeuvre pour créer au travers de ses solutions de collecte, d'achat et de vente de livre d'occasion de la valeur pour ses clients, l'environnement et le social afin de lutter contre le gaspillage et promouvoir l'économie circulaire en donnant une seconde vie au livres.</div>
                 </li>
                 <li>
                     <h3>Notre mission :</h3>
-                    <div>Ephesus a construit son ADN autour de 3 piliers pour lutter en faveur de la planète et de l'humain : l'environnement, le social et le sociétal. Notre librairie a pour mission de donner une seconde vie aux livres afin de réduire l’impact sur l’environnement et favoriser l’accès à la culture à un prix raisonnable.</div>
+                    <div><strong>Libres Livres</strong> a construit son ADN autour de 3 piliers pour lutter en faveur de la planète et de l'humain : l'environnement, le social et le sociétal. Notre librairie a pour mission de donner une seconde vie aux livres afin de réduire l’impact sur l’environnement et favoriser l’accès à la culture à un prix raisonnable.</div>
                 </li>
                 <li>
                     <h3>Nos services :</h3>
