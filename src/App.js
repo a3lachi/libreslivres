b677956327e6630ba32a56c41c8102c5e8d9e217
a3laxi <mad.aalachi@gmail.com>
2023-12-10 18:13:51 +0100

    updating meta data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
-      <Helmet>
-        <title>Libres Livres</title>
-        <meta name="libreslivres" content="echange de livres online" />
-      </Helmet>
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
